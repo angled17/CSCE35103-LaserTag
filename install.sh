51933988cdb89fa252b3bb37db2a4305c3126934
Angel Duron <aduron@uark.edu>
2026-02-15 20:54:09 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -9,6 +9,19 @@ sudo apt install make build-essential libssl-dev zlib1g-dev libbz2-dev libreadli
 # Install Pyenv
 curl https://pyenv.run | bash
 
+# Add To Bashrc
+echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bashrc
+echo '[[ -d $PYENV_ROOT/bin ]] && export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bashrc
+echo 'eval "$(pyenv init - bash)"' >> ~/.bashrc
+
+# Add to Bashprofile
+touch ~/.profile
+echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.profile
+echo '[[ -d $PYENV_ROOT/bin ]] && export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.profile
+echo 'eval "$(pyenv init - bash)"' >> ~/.profile
+
+soruce ~/.bashrc
+
 # Install Python 3.11.2
 pyenv install 3.11.2
 pyenv shell 3.11.2
