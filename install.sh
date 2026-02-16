8cf4ccc0e6aed271fe5ff8e54a20e97a8536040e
Angel Duron <aduron@uark.edu>
2026-02-15 21:00:53 -0600

    Fix install.sh

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@
 sudo apt update 
 
 # Install Pyenv dependencies. This is to ensure the right version of Python is used
-sudo apt install make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev curl git libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev
+sudo apt install make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev curl git libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev -y
 
 # Install Pyenv
 curl https://pyenv.run | bash
@@ -27,7 +27,7 @@ pyenv install 3.11.2
 pyenv shell 3.11.2
 
 # Install Tkinter
-sudo apt install python3-tk
+sudo apt install python3-tk -y
 
 # Create Virtual Environment
 python -m venv .venv
