d303a83b1ab4197c200266c46956759b5fa9e19e
Angel Duron <aduron@uark.edu>
2026-02-15 20:58:22 -0600

    Fix typo in install.sh

--- install.sh
+++ install.sh
@@ -20,7 +20,7 @@ echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.profile
 echo '[[ -d $PYENV_ROOT/bin ]] && export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.profile
 echo 'eval "$(pyenv init - bash)"' >> ~/.profile
 
-soruce ~/.bashrc
+source ~/.bashrc
 
 # Install Python 3.11.2
 pyenv install 3.11.2
